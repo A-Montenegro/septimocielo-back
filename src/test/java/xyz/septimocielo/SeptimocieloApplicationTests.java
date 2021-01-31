bbc6b7fe3f06fe97eba508c82ed7794961784433
Alberto Martínez Montenegro <albertomartinez44@hotmail.com>
2021-01-31 11:17:48 +0100

    Configuración integración contínua

--- src/test/java/xyz/septimocielo/SeptimocieloApplicationTests.java
+++ src/test/java/xyz/septimocielo/SeptimocieloApplicationTests.java
@@ -2,15 +2,9 @@ package xyz.septimocielo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 class SeptimocieloApplicationTests {
-
-	@ContextConfiguration(classes = SeptimocieloApplicationTests.class)
-	class ConfigClassApplicationContextTests {
-		// class body...
-	}
 
 	@Test
 	void contextLoads() {
